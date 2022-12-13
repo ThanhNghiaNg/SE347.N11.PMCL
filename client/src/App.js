7a93b58d2b9e0d026c6c36f7ecd8a31460858362
thanhnhan311201 <phannguyenthanhnhan3112@gmail.com>
2022-12-13 13:45:19 +0700

    complete UI

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,11 @@ import "./App.css";
 import Home from "./pages/Home";
 import Layout from "./components/Layout/Layout";
 
-import ModalForm from "./components/ModalForm/ModalForm";
-
 const App = () => {
   return (
-    // <Layout>
-    //   <Home/>
-    // </Layout>
-    <ModalForm />
+    <Layout>
+      <Home />
+    </Layout>
   );
 };
 
--- client/src/components/ModalForm/LoginForm/LoginForm.js
+++ client/src/components/ModalForm/LoginForm/LoginForm.js
@@ -2,17 +2,27 @@ import classes from "./LoginForm.module.css";
 
 const LoginForm = () => {
   return (
-    <form className="login-form">
-      <div className="login-form__email">
-        <input type="email" name="email" placeholder="Email đăng nhập" />
+    <div>
+      <div className={classes["login-form__heading"]}>
+        <h4>Đăng nhập bằng email</h4>
+        <p>Nhập email và mật khẩu tài khoản tiki</p>
       </div>
-      <div className="login-form__password">
-        <input type="password" name="login-password" placeholder="Mật khẩu" />
+      <form className={classes["login-form"]}>
+        <div className={classes["login-form__email"]}>
+          <input type="email" name="email" placeholder="Email đăng nhập" />
+        </div>
+        <div className={classes["login-form__password"]}>
+          <input type="password" name="login-password" placeholder="Mật khẩu" />
+        </div>
+        <div className={classes["login-form__actions"]}>
+          <button>ĐĂNG NHẬP</button>
+        </div>
+      </form>
+
+      <div className={classes["login-form__reset"]}>
+        <p>Quên mật khẩu</p>
       </div>
-      <div className="login-form__actions">
-        <button>ĐĂNG NHẬP</button>
-      </div>
-    </form>
+    </div>
   );
 };
 
