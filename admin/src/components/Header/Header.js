6f0231160fd7848fcead37ceb71b221d7747c6c7
JTNghia <nghiant089@gmail.com>
2022-12-17 02:07:04 +0700

    add admin auth

--- admin/src/components/Header/Header.js
+++ admin/src/components/Header/Header.js
@@ -1,32 +1,59 @@
 import { Link } from "react-router-dom";
 import Container from "../UI/Container";
 import classes from "./Header.module.css";
+import authContext from "../../store/authContext";
+import { useContext } from "react";
+import { hostURL } from "../../utils/global";
 
 const Header = (props) => {
+  const authCtx = useContext(authContext);
+
+  const logoutHandler = (event) => {
+    event.preventDefault();
+    const postLogout = async()=>{
+      const respone = await fetch(`${hostURL}/admin-logout`,{
+        credentials: "include",
+        method: "POST",
+      })
+      if (respone.status === 200){
+        authCtx.logout()
+      }
+    }
+    postLogout()
+  };
+
   return (
     <Container className={classes.header}>
-      <nav class={classes.nav}>
+      <nav className={classes.nav}>
+        {authCtx.isLoggedIn && (
+          <ul className={classes["nav-items"]}>
+            <li>
+              <Link to="/">Books</Link>
+            </li>
+            <li>
+              <Link to="/add-book">Add book</Link>
+            </li>
+            <li>
+              <Link to="/orders">Orders</Link>
+            </li>
+          </ul>
+        )}
         <ul className={classes["nav-items"]}>
-          <li>
-            <Link to="/">Books</Link>
-          </li>
-          <li>
-            <Link to="/add-book">Add book</Link>
-          </li>
-          <li>
-            <Link to="/orders">Orders</Link>
-          </li>
-        </ul>
-        <ul className={classes["nav-items"]}>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-          <li>
-            <Link>Logout</Link>
-          </li>
+          {!authCtx.isLoggedIn && (
+            <>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+            </>
+          )}
+          {authCtx.isLoggedIn && (
+            <li>
+              <Link onClick={logoutHandler}>Logout</Link>
+            </li>
+          )}
         </ul>
       </nav>
     </Container>
--- server/routes/admin.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const route = express.Router();
-
-route.get("/add", (req, res, next) => {});
-
-module.exports = route;
