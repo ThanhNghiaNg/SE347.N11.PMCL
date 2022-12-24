d5cbd6686c21621e67bfff0d6853c28950eded99
ThanhNghiaNg <nghiant089@gmail.com>
2022-12-24 18:17:12 +0700

    client add style footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,14 +1,23 @@
-import classes from './Footer.module.css'
-import Container from '../UI/Container'
-const Footer = props=>{
-    return <Container className={classes.footer}>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
+import classes from "./Footer.module.css";
+import Container from "../UI/Container";
+const Footer = (props) => {
+  const clickGithubHandler = (event) => {
+    event.preventDefault();
+    window.open("https://github.com/thanhnhan311201");
+    window.open("https://github.com/ThanhNghiaNg");
+  };
+  return (
+    <Container className={classes.footer}>
+      <ul className="d-flex justify-content-center w-100">
+        <li>
+          <a onClick={clickGithubHandler}>
+            <i class="fa-brands fa-github"></i>
+          </a>
+        </li>
+        <li></li>
+      </ul>
     </Container>
-    
-}
+  );
+};
 
-export default Footer
+export default Footer;
